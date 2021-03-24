f11b10967c4f035610c127aea45492cd829a229e
Kamil Golec <kumalgfilms@gmail.com>
2021-03-24 21:56:47 +0100

    better dayjs localization

--- src/functions/dayjs.js
+++ src/functions/dayjs.js
@@ -13,10 +13,10 @@ dayjs.updateLocale('pl', {
   calendar: {
     sameDay: '[dzisiaj]', // The same day ( Today at 2:30 AM )
     nextDay: '[jutro]', // The next day ( Tomorrow at 2:30 AM )
-    nextWeek: 'dddd', // The next week ( Sunday at 2:30 AM )
+    nextWeek: 'dddd, D MMM', // The next week ( Sunday at 2:30 AM )
     // lastDay: '[Wczoraj]', // The day before ( Yesterday at 2:30 AM )
     // lastWeek: '[Poprzedni] dddd', // Last week ( Last Monday at 2:30 AM )
-    sameElse: 'DD-MM-YYYY' // Everything else ( 17/10/2011 )
+    sameElse: 'DD MMMM' // Everything else ( 17/10/2011 )
   }
 })
 
