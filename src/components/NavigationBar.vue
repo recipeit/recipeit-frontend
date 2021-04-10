567851a0f0ef7c05f4a785acecc51dd6a130df6a
Kamil Golec <kumalgfilms@gmail.com>
2021-04-10 23:13:38 +0200

    navigation better click

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="navigation-bar">
     <router-link :to="{ name: 'home' }" class="navigation-bar__item">
-      <BaseIcon icon="home" />
+      <BaseIcon class="navigation-bar__item__icon" icon="home" />
       <span>Kokpit</span>
     </router-link>
     <router-link :to="{ name: 'recipes' }" class="navigation-bar__item">
-      <BaseIcon icon="cookbook" />
+      <BaseIcon class="navigation-bar__item__icon" icon="cookbook" />
       <span>Przepisy</span>
     </router-link>
     <router-link :to="{ name: 'cook-it' }" class="navigation-bar__item">
-      <!-- <BaseIcon icon="chef-hat" weight="fill" /> -->
-      <BaseIcon icon="chef-hat" />
+      <!-- <BaseIcon class="navigation-bar__item__icon"icon="chef-hat" weight="fill" /> -->
+      <BaseIcon class="navigation-bar__item__icon" icon="chef-hat" />
       <span>Ugotuj to!</span>
     </router-link>
     <router-link :to="{ name: 'my-kitchen' }" class="navigation-bar__item">
-      <BaseIcon icon="food" />
+      <BaseIcon class="navigation-bar__item__icon" icon="food" />
       <span>Moja kuchnia</span>
     </router-link>
     <router-link :to="{ name: 'shopping-list' }" class="navigation-bar__item">
-      <BaseIcon icon="basket" />
+      <BaseIcon class="navigation-bar__item__icon" icon="basket" />
       <span>Lista zakupów</span>
     </router-link>
   </div>
@@ -35,7 +35,8 @@ export default {}
   left: 0;
   right: 0;
   bottom: 0;
-  padding: 28px 0;
+  // padding: 28px 0;
+  padding: 0 5%;
   background-color: rgba(var(--color-background-rgb), 0.95);
   display: flex;
   justify-content: space-evenly;
@@ -46,12 +47,19 @@ export default {}
   }
 
   &__item {
+    flex: 1;
+    padding: 28px 0;
+    text-align: center;
     font-size: 1.5rem;
     color: var(--color-link-text-primary-color);
     // transition: color 0.3s ease;
     position: relative;
     line-height: 0;
-    @include transition((color, transform));
+    // @include transition((color, transform));
+
+    &__icon {
+      @include transition((color, transform));
+    }
 
     span {
       display: none;
@@ -78,16 +86,19 @@ export default {}
     //   transition: all 0.3s ease;
     // }
 
-    &.router-link-exact-active {
+    &.router-link-exact-active &__icon {
       color: var(--color-primary);
       transform: scale(1.5);
+
+      // ::v-deep(svg) {
+      // }
       // filter: drop-shadow(0 0 4px #fff);
 
-      &:after {
-        background-color: var(--color-primary);
-        left: 0;
-        right: 0;
-      }
+      // &:after {
+      //   background-color: var(--color-primary);
+      //   left: 0;
+      //   right: 0;
+      // }
     }
   }
 }
