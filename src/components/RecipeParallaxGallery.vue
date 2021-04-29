e16127c8588e96d1d05ee4bfe01c77aca8ae3523
Kamil Golec <kumalgfilms@gmail.com>
2021-04-29 23:07:33 +0200

    refactored gallery

--- src/components/RecipeParallaxGallery.vue
+++ src/components/RecipeParallaxGallery.vue
@@ -1,44 +1,29 @@
 <template>
-  <div ref="block" :class="globalClasses" :style="globalStyles">
-    <div class="recipe-parallax-gallery__controls">
-      <button
-        class="recipe-parallax-gallery__controls__left"
-        @click.prevent="changeCurrentToPrevious()"
-        :disabled="isCurrentImageFirst ? true : null"
-      >
+  <div ref="block" class="recipe-parallax-gallery">
+    <div class="gallery-controls">
+      <button class="gallery-control-left" @click.prevent="showPrev()" :disabled="isCurrentImageFirst ? true : null">
         <BaseIcon icon="angle-left" />
       </button>
-      <button
-        class="recipe-parallax-gallery__controls__right"
-        @click.prevent="changeCurrentToNext()"
-        :disabled="isCurrentImageLast ? true : null"
-      >
+      <button class="gallery-control-right" @click.prevent="showNext()" :disabled="isCurrentImageLast ? true : null">
         <BaseIcon icon="angle-right" />
       </button>
     </div>
-    <div class="recipe-parallax-gallery__indicators">
+    <div class="gallery-indicators">
       <span
-        :class="['recipe-parallax-gallery__indicator', { 'recipe-parallax-gallery__indicator--current': currentImageIndex === index }]"
-        v-for="(_, index) in imageObjects"
+        :class="['gallery-indicator', { 'gallery-indicator--current': currentImageIndex === index }]"
+        v-for="(_, index) in images"
         :key="index"
+        @click="scrollTo(index)"
       />
     </div>
-    <div ref="images" class="recipe-parallax-gallery__images">
-      <BaseImageLazyload
-        class="recipe-parallax-gallery__image"
-        :id="image.id"
-        v-for="(image, index) in imageObjects"
-        :ref="setImageRef($event)"
-        :src="image.src"
-        :key="index"
-        @click="move(index)"
-      />
+    <div ref="images" class="gallery-images">
+      <BaseImageLazyload class="gallery-image" v-for="(image, index) in images" :key="index" :src="image.src" :ref="setImageRef" />
     </div>
   </div>
 </template>
 
 <script>
-import uniqueID from '@/functions/uniqueID'
+import debounce from 'lodash.debounce'
 
 export default {
   props: {
@@ -49,11 +34,6 @@ export default {
   },
   data() {
     return {
-      changingCurrentIndex: false,
-      preparingChangingCurrentIndex: false,
-      isPointerDown: false,
-      offset: null,
-      movement: 0,
       currentImageIndex: 0,
       imageRefs: []
     }
@@ -61,164 +41,60 @@ export default {
   beforeUpdate() {
     this.imageRefs = []
   },
-  beforeMount() {
-    window.addEventListener('scroll', this.scrollHandler, false)
-  },
   mounted() {
-    this.scrollHandler()
+    this.imagesScrollHandler = debounce(this.imagesScrollHandlerDebounced, 200)
+    this.windowScrollHandler()
+
+    window.addEventListener('scroll', this.windowScrollHandler, false)
+    this.$refs.images.addEventListener('scroll', this.imagesScrollHandler, false)
   },
   beforeUnmount() {
-    window.removeEventListener('scroll', this.scrollHandler, false)
+    window.removeEventListener('scroll', this.windowScrollHandler, false)
+    this.$refs.images.removeEventListener('scroll', this.imagesScrollHandler, false)
   },
   methods: {
-    setImageRef(el) {
-      if (el) {
-        this.imageRefs.push(el)
+    setImageRef({ $el }) {
+      this.imageRefs.push($el)
+    },
+    showPrev() {
+      if (this.currentImageIndex > 0) {
+        this.scrollTo(this.currentImageIndex - 1)
       }
     },
-    scrollHandler() {
+    showNext() {
+      if (this.currentImageIndex < this.images.length) {
+        this.scrollTo(this.currentImageIndex + 1)
+      }
+    },
+    scrollTo(index) {
+      if (!this.imageRefs) return
+      const imageEl = this.imageRefs[index]
+      if (imageEl) {
+        imageEl.scrollIntoView({
+          behavior: 'smooth'
+        })
+        if (this.currentImageIndex !== index) {
+          this.currentImageIndex = index
+        }
+      }
+    },
+    windowScrollHandler() {
       const parentHeight = this.$refs.block.offsetHeight
       const scrollFactor = Math.max(0, Math.min(window.pageYOffset / parentHeight, 1))
       const transformValue = (scrollFactor * parentHeight) / 2
       this.$refs.images.style.transform = `translate3d(0, ${transformValue}px ,0)`
     },
-    imageClasses(index) {
-      return [
-        'recipe-parallax-gallery__image',
-        {
-          'recipe-parallax-gallery__image--current': this.currentImageIndex === index,
-          'recipe-parallax-gallery__image--previous': this.currentImageIndex - 1 === index,
-          'recipe-parallax-gallery__image--next': this.currentImageIndex + 1 === index
-        }
-      ]
-    },
-    mouseDownHandler(event) {
-      if (this.preparingChangingCurrentIndex) return
-      this.isPointerDown = true
-      this.offset = [event.clientX, event.clientY]
-    },
-    mouseMoveHandler(event) {
-      if (this.isPointerDown && this.offset) {
-        event.preventDefault()
-        this.onPointerMove({
-          x: event.clientX,
-          y: event.clientY
-        })
-      }
-    },
-    mouseUpHandler() {
-      this.onPointerUp()
-    },
-    touchStartHanlder(event) {
-      if (this.preparingChangingCurrentIndex) return
-      this.isPointerDown = true
-      this.offset = [event.changedTouches[0].clientX, event.changedTouches[0].clientY]
-    },
-    touchMoveHandler(event) {
-      if (this.isPointerDown && this.offset) {
-        event.preventDefault()
-        this.onPointerMove({
-          x: event.changedTouches[0].clientX,
-          y: event.changedTouches[0].clientY
-        })
-      }
-    },
-    touchEndHandler() {
-      this.onPointerUp()
-    },
-    onPointerMove(mousePosition) {
-      this.movement = {
-        value: mousePosition.x - this.offset[0],
-        unit: 'px'
-      }
-      if (this.isCurrentImageFirst && this.movement.value > 0) {
-        this.movement.value = 0
-      } else if (this.isCurrentImageLast && this.movement.value < 0) {
-        this.movement.value = 0
-      }
-    },
-    onPointerUp() {
-      if (this.movement.value > 128) {
-        this.changeCurrentToPrevious()
-      } else if (this.movement.value < -128) {
-        this.changeCurrentToNext()
-      } else {
-        this.movement = null
-      }
-      this.isPointerDown = false
-      this.offset = null
-    },
-    transitionEndPrevHandler(e) {
-      if (e.target.id === this.imageObjects[this.currentImageIndex - 1]?.id) {
-        this.changingCurrentIndex = true
-        this.$refs.images.removeEventListener('transitionend', this.transitionEndPrevHandler, false)
-        this.currentImageIndex--
-        this.movement = null
-        setTimeout(() => {
-          this.changingCurrentIndex = false
-          this.preparingChangingCurrentIndex = false
-        }, 0)
-      }
-    },
-    transitionEndNextHandler(e) {
-      if (e.target.id === this.imageObjects[this.currentImageIndex + 1]?.id) {
-        this.changingCurrentIndex = true
-        this.$refs.images.removeEventListener('transitionend', this.transitionEndNextHandler, false)
-        this.currentImageIndex++
-        this.movement = null
-        setTimeout(() => {
-          this.changingCurrentIndex = false
-          this.preparingChangingCurrentIndex = false
-        }, 0)
-      }
-    },
-    changeCurrentToPrevious() {
-      if (this.isCurrentImageFirst) return
-      this.$refs.images.addEventListener('transitionend', this.transitionEndPrevHandler, false)
-      this.preparingChangingCurrentIndex = true
-      this.movement = {
-        value: 100,
-        unit: '%'
-      }
-    },
-    changeCurrentToNext() {
-      if (this.isCurrentImageLast) return
-      this.$refs.images.addEventListener('transitionend', this.transitionEndNextHandler, false)
-      this.preparingChangingCurrentIndex = true
-      this.movement = {
-        value: -100,
-        unit: '%'
+    imagesScrollHandlerDebounced() {
+      const { scrollLeft, scrollWidth } = this.$refs.images
+      let index = Math.round((scrollLeft / scrollWidth) * this.images.length)
+      if (this.currentImageIndex !== index) {
+        this.currentImageIndex = index
       }
     }
   },
   computed: {
-    globalClasses() {
-      return [
-        'recipe-parallax-gallery',
-        { 'recipe-parallax-gallery--pointer-down': this.isPointerDown },
-        { 'recipe-parallax-gallery--changing-current-index': this.changingCurrentIndex }
-      ]
-    },
-    globalStyles() {
-      if (this.movement) {
-        return {
-          '--recipe-parallax-gallery-movement': `${this.movement.value}${this.movement.unit}`
-        }
-      }
-      return {
-        '--recipe-parallax-gallery-movement': '0px'
-      }
-    },
-    imageObjects() {
-      return this.images.map(i => {
-        return {
-          id: `parallax-image-${uniqueID().getID()}`,
-          ...i
-        }
-      })
-    },
     isCurrentImageLast() {
-      return this.currentImageIndex + 1 === this.imageObjects.length
+      return this.currentImageIndex + 1 === this.images.length
     },
     isCurrentImageFirst() {
       return this.currentImageIndex === 0
@@ -227,7 +103,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .recipe-parallax-gallery {
   $root: &;
 
@@ -235,15 +111,15 @@ export default {
   position: relative;
   overflow: hidden;
 
-  &__controls {
+  .gallery-controls {
     display: none;
 
     @include cursor-only {
       display: initial;
     }
 
-    &__left,
-    &__right {
+    .gallery-control-left,
+    .gallery-control-right {
       position: absolute;
       top: 50%;
       z-index: 10;
@@ -272,16 +148,16 @@ export default {
       }
     }
 
-    &__left {
+    .gallery-control-left {
       left: 32px;
     }
 
-    &__right {
+    .gallery-control-right {
       right: 32px;
     }
   }
 
-  &__indicators {
+  .gallery-indicators {
     position: absolute;
     bottom: 3rem;
     z-index: 1;
@@ -289,21 +165,29 @@ export default {
     transform: translateX(-50%);
   }
 
-  &__indicator {
+  .gallery-indicator {
     display: inline-block;
     width: 0.75rem;
     height: 0.75rem;
     margin: 0.25rem;
     background-color: rgba(var(--color-background-rgb), 0.5);
     border-radius: 50px;
+    cursor: pointer;
     @include transition(background-color);
+
+    @include cursor-only {
+      &:hover {
+        background-color: rgba(var(--color-background-rgb), 0.95);
+      }
+    }
 
     &--current {
       background-color: rgba(var(--color-background-rgb), 0.95);
+      cursor: initial;
     }
   }
 
-  &__images {
+  .gallery-images {
     width: 100%;
     height: 100%;
     transform: translate3d(0, 0, 0);
@@ -316,46 +200,18 @@ export default {
     overflow-x: scroll;
   }
 
-  &__image {
+  .gallery-image {
     flex-shrink: 0;
     scroll-snap-align: start;
     width: 100%;
     height: 100%;
-    // display: none;
+    scroll-snap-stop: always;
 
-    img {
+    ::v-deep(img) {
       object-fit: cover;
       width: 100%;
       height: 100%;
     }
-
-    // &--previous,
-    // &--current,
-    // &--next {
-    //   display: block;
-    //   transform: translate3d(var(--recipe-parallax-gallery-movement), 0, 0);
-    //   @include transition(transform, 0.15s, ease);
-
-    //   #{ $root }--pointer-down &,
-    //   #{ $root }--changing-current-index & {
-    //     transition-duration: 0s;
-    //   }
-    // }
-
-    // &--previous,
-    // &--next {
-    //   position: absolute;
-    // }
-
-    // &--previous {
-    //   top: 0;
-    //   right: 100%;
-    // }
-
-    // &--next {
-    //   top: 0;
-    //   left: 100%;
-    // }
   }
 }
 </style>
