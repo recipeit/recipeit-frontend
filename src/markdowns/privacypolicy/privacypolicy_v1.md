53309d8b909d8210588657cc7ce908c1572ad406
Kamil Golec <kumalgfilms@gmail.com>
2021-05-11 02:12:07 +0200

    update terms styles

--- src/markdowns/privacypolicy/privacypolicy_v1.md
+++ src/markdowns/privacypolicy/privacypolicy_v1.md
@@ -1,8 +1,8 @@
 # Polityka Prywatności
 
-## Dane, jakie gromadzimy
+## 1. Dane, jakie gromadzimy
 
-### Wykorzystywane ciasteczka
+### 1.1. Wykorzystywane ciasteczka
 
 - **X_ACCESS_TOKEN, X_REFRESH_TOKEN** - logowanie, trzymanie sesji użytkownika
 - **\_GRECAPTCHA** - weryfikacja formularzy
@@ -10,8 +10,8 @@
 - **recipeit_cookies_message** - ciasteczko które jest używane do określenia, czy użytkownik przeczytał komunikat dotyczący ciasteczek na stronie
 - **recipeit_cookies_analytics** - ciasteczko, które jest ustawiane w momencie uzyskania od użytkownika zgody na wykorzystywanie Google Analytics na stronie
 
-## W jakim celu wykorzystujemy Państwa dane
+## 2. W jakim celu wykorzystujemy Państwa dane
 
-## Bezpieczeństwo danych
+## 3. Bezpieczeństwo danych
 
-## Dzieci w wieku poniżej 16 lat / osoby niepełnoletnie
+## 4. Dzieci w wieku poniżej 16 lat / osoby niepełnoletnie
--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -20,31 +20,47 @@ export default {
 
 <style lang="scss" scoped>
 .privacy-policy {
-  font-size: 0.875rem;
+  font-size: 0.75rem;
   line-height: 1.75;
 
-  ul {
-    list-style: initial;
-    padding-left: 2rem;
-    margin: 1rem 0;
+  ::v-deep(h1) {
+    font-size: 1.25rem;
   }
 
-  li + li {
-    margin-top: 1rem;
+  ::v-deep(h2) {
+    font-size: 1rem;
+    margin: 1.5rem 0;
   }
-}
 
-.h1 {
-  font-size: 1.25rem;
-}
+  ::v-deep(h3) {
+    font-size: 0.875rem;
+    margin: 1.5rem 0;
+  }
 
-.h2 {
-  font-size: 1rem;
-  margin-top: 2rem;
-}
+  ::v-deep(ol) {
+    counter-reset: list;
+  }
 
-.h3 {
-  font-size: 0.875rem;
-  margin-top: 2rem;
+  ::v-deep(ol > li) {
+    position: relative;
+    list-style: none;
+  }
+
+  ::v-deep(ol > li::before) {
+    position: absolute;
+    left: -24px;
+    content: counter(list, decimal) '. ';
+    counter-increment: list;
+    font-weight: bold;
+  }
+
+  ::v-deep(ul) {
+    list-style: disc;
+    padding-inline-start: 40px;
+  }
+
+  ::v-deep(ul > li) {
+    margin: 0.5rem 0;
+  }
 }
 </style>
