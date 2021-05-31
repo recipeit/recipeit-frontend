5ce8e7b577069ae7409cb155531c382c12601cb1
Kamil Golec <kumalgfilms@gmail.com>
2021-06-01 00:07:41 +0200

    fix config

--- vue.config.js
+++ vue.config.js
@@ -102,11 +102,11 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     iconPaths: {
-      favicon32: '/img/icons/favicon-32x32.png',
-      favicon16: '/img/icons/favicon-16x16.png',
-      appleTouchIcon: '/img/icons/apple-touch-icon-152x152.png',
-      maskIcon: '/img/icons/safari-pinned-tab.svg',
-      msTileImage: '/img/icons/msapplication-icon-144x144.png'
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     }
   }
 }
