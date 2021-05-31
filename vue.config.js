211fd06991ae68863485350621c8328206e85944
Kamil Golec <kumalgfilms@gmail.com>
2021-05-31 23:35:55 +0200

    manifest update

--- vue.config.js
+++ vue.config.js
@@ -65,10 +65,48 @@ module.exports = {
     }
   },
   pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
+    manifestOptions: {
+      name: 'Recipeit',
+      short_name: 'Recipeit',
+      start_url: '.',
+      theme_color: '#FFFFFF',
+      display: 'standalone',
+      background_color: '#FFFFFF',
+      icons: [
+        {
+          src: '/img/icons/android-chrome-192x192.png',
+          type: 'image/png',
+          sizes: '192x192'
+        },
+        {
+          src: '/img/icons/android-chrome-512x512.png',
+          type: 'image/png',
+          sizes: '512x512'
+        },
+        {
+          src: '/img/icons/android-chrome-maskable-192x192.png',
+          type: 'image/png',
+          sizes: '192x192',
+          purpose: 'maskable'
+        },
+        {
+          src: '/img/icons/android-chrome-maskable-512x512.png',
+          type: 'image/png',
+          sizes: '512x512',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    themeColor: '#FFFFFF',
+    msTileColor: '#FFFFFF',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black'
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: '/img/icons/favicon-32x32.png',
+      favicon16: '/img/icons/favicon-16x16.png',
+      appleTouchIcon: '/img/icons/apple-touch-icon-152x152.png',
+      maskIcon: '/img/icons/safari-pinned-tab.svg',
+      msTileImage: '/img/icons/msapplication-icon-144x144.png'
+    }
   }
 }
