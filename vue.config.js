d1c0d175bc7c153a190e055df06a26a92fbf84b1
Kamil Golec <kumalgfilms@gmail.com>
2021-06-02 12:18:26 +0200

    maskable change name

--- vue.config.js
+++ vue.config.js
@@ -84,13 +84,13 @@ module.exports = {
           sizes: '512x512'
         },
         {
-          src: '/img/icons/android-chrome-maskable-192x192.png',
+          src: '/img/icons/pwa-maskable-192x192.png',
           type: 'image/png',
           sizes: '192x192',
           purpose: 'any maskable'
         },
         {
-          src: '/img/icons/android-chrome-maskable-512x512.png',
+          src: '/img/icons/pwa-maskable-512x512.png',
           type: 'image/png',
           sizes: '512x512',
           purpose: 'any maskable'
