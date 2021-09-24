310fd3382cc99369c29006c039bb6764db294222
Kamil Golec <kumalgfilms@gmail.com>
2021-09-25 00:12:25 +0200

    social test

--- src/configs/socialMedia.js
+++ src/configs/socialMedia.js
@@ -1,2 +1,2 @@
-export const FACEBOOK_PROFILE_URL = 'https://www.facebook.com/RecipeitPL'
-export const INSTAGRAM_PROFILE_URL = 'https://www.instagram.com/recipeitpl'
+export const FACEBOOK_PROFILE_URL = 'https://facebook.com/RecipeitPL'
+export const INSTAGRAM_PROFILE_URL = 'https://instagram.com/recipeitpl'
