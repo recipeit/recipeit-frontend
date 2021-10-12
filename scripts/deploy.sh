9d5fda0d3920261b115175d9fcff9508bf06b40f
Jakub Kozłowski <kubukoz@gmail.com>
2021-10-12 16:28:14 +0200

    Deploy from branches

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,20 @@
 set -e
 set -o pipefail
 
+# Checkout main before pull
+git \
+  -C app/src/recipeit-frontend \
+  checkout main
+
+# Pull changes
 git \
   -C app/src/recipeit-frontend \
   pull
+
+# Switch to branch/tag being built
+git \
+  -C app/src/recipeit-frontend \
+  checkout "${GITHUB_BRANCH:-$GITHUB_SHA}"
 
 docker-compose \
   -f app/src/docker-compose.yml \
