48fb57d9592d4a2b7f22c83d647b874d8058bf30
Kamil Golec <kumalgfilms@gmail.com>
2021-11-26 16:43:09 +0100

    fix instagram link

--- src/configs/socialMedia.js
+++ src/configs/socialMedia.js
@@ -1,2 +1,2 @@
-export const FACEBOOK_PROFILE_URL = 'https://www.facebook.com/RecipeitPL'
-export const INSTAGRAM_PROFILE_URL = 'https://www.instagram.com/recipeit.pl'
+export const FACEBOOK_PROFILE_URL = 'https://www.facebook.com/RecipeitPL/'
+export const INSTAGRAM_PROFILE_URL = 'https://www.instagram.com/recipeit.pl/'
