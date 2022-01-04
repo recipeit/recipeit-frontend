d561734c6ca50026ec4f3392860cd4b070e44fb2
Kamil Golec <kumalgfilms@gmail.com>
2022-01-04 23:39:51 +0100

    fix images

--- src/configs/recipeCategories.js
+++ src/configs/recipeCategories.js
@@ -1,17 +1,31 @@
 import recipeCategories from '@/constants/recipeCategories'
 
+import breakfastImage from '@/assets/img/categories/breakfast.webp'
+import dinnerImage from '@/assets/img/categories/dinner.webp'
+import supperImage from '@/assets/img/categories/supper.webp'
+
+import cakeImage from '@/assets/img/categories/cake.webp'
+import dessertImage from '@/assets/img/categories/dessert.webp'
+import snackImage from '@/assets/img/categories/snack.webp'
+import pastaImage from '@/assets/img/categories/pasta.webp'
+import saladImage from '@/assets/img/categories/salad.webp'
+import casseroleImage from '@/assets/img/categories/casserole.webp'
+
+import cuisineItalianImage from '@/assets/img/categories/cuisine-italian.webp'
+import cuisineThaiImage from '@/assets/img/categories/cuisine-thai.webp'
+
 export const CATEGORY_IMAGES = {
-  [recipeCategories.BREAKFAST]: import('@/assets/img/categories/breakfast.webp'),
-  [recipeCategories.DINNER]: import('@/assets/img/categories/dinner.webp'),
-  [recipeCategories.SUPPER]: import('@/assets/img/categories/supper.webp'),
+  [recipeCategories.BREAKFAST]: breakfastImage,
+  [recipeCategories.DINNER]: dinnerImage,
+  [recipeCategories.SUPPER]: supperImage,
 
-  [recipeCategories.CAKE]: import('@/assets/img/categories/cake.webp'),
-  [recipeCategories.DESSERT]: import('@/assets/img/categories/dessert.webp'),
-  [recipeCategories.SNACK]: import('@/assets/img/categories/snack.webp'),
-  [recipeCategories.PASTA]: import('@/assets/img/categories/pasta.webp'),
-  [recipeCategories.SALAD]: import('@/assets/img/categories/salad.webp'),
-  [recipeCategories.CASSEROLE]: import('@/assets/img/categories/casserole.webp'),
+  [recipeCategories.CAKE]: cakeImage,
+  [recipeCategories.DESSERT]: dessertImage,
+  [recipeCategories.SNACK]: snackImage,
+  [recipeCategories.PASTA]: pastaImage,
+  [recipeCategories.SALAD]: saladImage,
+  [recipeCategories.CASSEROLE]: casseroleImage,
 
-  [recipeCategories.CUISINE_ITALIAN]: import('@/assets/img/categories/cuisine-italian.webp'),
-  [recipeCategories.CUISINE_THAI]: import('@/assets/img/categories/cuisine-thai.webp')
+  [recipeCategories.CUISINE_ITALIAN]: cuisineItalianImage,
+  [recipeCategories.CUISINE_THAI]: cuisineThaiImage
 }
