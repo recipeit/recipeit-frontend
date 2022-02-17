df8f6663439eaf090a23518ec1490a27ea505459
Kamil Golec <kumalgfilms@gmail.com>
2022-02-17 22:26:25 +0100

    end of line

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,6 @@ module.exports = {
   singleQuote: true,
   semi: false,
   printWidth: 140,
-  endOfLine: 'auto',
   trailingComma: 'none',
   arrowParens: 'avoid',
   htmlWhitespaceSensitivity: 'strict'
